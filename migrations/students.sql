3d5d7d8edd92d4e9c3145e3c8dbce253e436f8c2
Muflih Kholidin <muflic.24@gmail.com>
2017-11-17 21:35:39 +0700

    Add sql migration

---
+++ migrations/students.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `students` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
